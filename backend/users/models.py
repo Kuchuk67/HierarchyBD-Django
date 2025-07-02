9f5ba8e9e2204f91c464323d726162d654d7f50a
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2025-07-02 09:00:50 +0300

    init users

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
+    def __str__(self):
+        return self.email
+
+
+    def __repr__(self):
+        return f"{self.pk} {self.email}"
+
