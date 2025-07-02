1b3f0b5c006da0e7fabd23cbad0e232584fd5d2e
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2025-07-02 11:18:12 +0300

    permision for group

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = [MY_HOST]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    # 'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
     'users',
     #'partnerships',
 ]
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- backend/users/management/commands/add_user.py
+++ backend/users/management/commands/add_user.py
@@ -1,11 +1,32 @@
 from django.core.management.base import BaseCommand
 from users.models import CustomUser
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.sessions.models import Session
+from getpass import getpass
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        # CustomUser.objects.all().delete()
+        CustomUser.objects.all().delete()
+
+        # Создаем новую группу
+        new_group, created = Group.objects.get_or_create(name="API_access")
+
+        print("Создаем новую группу", new_group, created)
+
+        if created:
+            ct = ContentType.objects.get_for_model(Session)
+            #
+            permission = Permission.objects.create(
+               codename="API_access",
+               name="Имеет доступ к API",
+               content_type=ct,
+            )
+            new_group.permissions.add(permission)
+
+            print("Подключили пермишены")
 
         # Создаем суперпользователя
         email = input('Enter your e-mail: ')
@@ -14,16 +35,21 @@ class Command(BaseCommand):
             email=email,
         )
         if not user:
-            user = CustomUser.objects.create(
+            user = CustomUser._default_manager.create(
                 email=email,
             )
-            password = input('Enter password: ')
-            password2 = input('Enter password repeated: ')
+            print('Enter password: ')
+            # Пароль не отображается при вводе
+            password = getpass()
+            print('Enter password repeated: ')
+            # Пароль не отображается при вводе
+            password2 = getpass()
             if password == password2:
                 user.set_password(password)
                 user.is_active = True
                 user.is_staff = True
                 user.is_superuser = True
+                user.groups.add(new_group)
                 user.save()
             else:
                 print('Рasswords do not match')
