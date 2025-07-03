99a756a721b0e2e111559ae1440244036d80ef79
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2025-07-03 13:55:08 +0300

    end partner

--- backend/counterparties/views.py
+++ backend/counterparties/views.py
@@ -1,15 +1,16 @@
 from rest_framework.generics import ListAPIView
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         PermissionRequiredMixin)
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ViewSet
 from counterparties.models import Counterparties
 from counterparties.serializer import CounterpartiesSerializer
-from rest_framework.permissions import IsAuthenticated
+#from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.response import Response
 from rest_framework import status
-from users.permissions import HasAPIGroupPermission
+#from users.permissions import HasAPIGroupPermission
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 
 
 class CounterpartiesViewsSet(ModelViewSet):
@@ -20,7 +21,8 @@ class CounterpartiesViewsSet(ModelViewSet):
     # pagination_class = EducationPagination
     queryset = Counterparties.objects.all()
     serializer_class = CounterpartiesSerializer
-    permission_classes = [IsAuthenticated, HasAPIGroupPermission]
+    #permission_classes = [IsAuthenticated, HasAPIGroupPermission]
+    
 
 
     def destroy(self, request, *args, **kwargs):
@@ -36,7 +38,7 @@ class CounterpartiesViewsSet(ModelViewSet):
         serializer = self.get_serializer(instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def list(self, request, *args, **kwargs):
+    def list(self, request, deactive = False, *args, **kwargs):
         """
         При выводе списка отображаем только активных контрагентов
         """
@@ -49,20 +51,26 @@ class CounterpartiesViewsSet(ModelViewSet):
 
         serializer = self.get_serializer(parties, many=True)
         return Response(serializer.data)
-
-    @action(detail=False)
-    def all(self, request):
-        """
-        Дополнительный list
-        При выводе списка отображаем и неактивных контрагентов
-        """
-        parties = Counterparties.objects.all().order_by('name')
-
-        page = self.paginate_queryset(parties)
+    
+    # Декоратор для отображения Swagger параметров пагинации
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(name='limit', type=int, location=OpenApiParameter.QUERY, description='Limit items'),
+            OpenApiParameter(name='offset', type=int, location=OpenApiParameter.QUERY, description='Offset'),
+        ],
+        responses=CounterpartiesSerializer(many=True),
+    )
+    @action(detail=False, methods=["get"], url_path="deactive")
+    def deactive(self, request):
+        queryset = self.get_queryset().filter(active=False)
+        page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
-        serializer = self.get_serializer(parties, many=True)
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+
+
 
